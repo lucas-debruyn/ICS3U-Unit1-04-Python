3de44220bb63c6d96ce4ebbfc0cf0a833f3d1666
Lucas DeBruyn <lucas.debruyn@mths.ca>
2022-09-20 14:50:46 +0000

    Added "Done"

---
+++ basic_math.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Created by: Lucas DeBruyn
+# Created on: Sep 2020
+# This program calculates the area and perimeter of a rectangle
+#    with dimensions 5cm x 3cm
+
+
+def main():
+    # this function calculates the area and perimeter
+
+    print("If a rectangle has the dimensions: ")
+    print("5cm x 3cm")
+    print("")
+    print("Area is {}cm².".format(3 * 5))
+    print("Perimeter is {}cm.".format(2 * (3 + 5)))
+
+    print("\nDone.")
+
+if __name__ == "__main__":
+    main()
